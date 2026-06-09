f5ceb74b532d6109b89ce13311a2193f21c6e721
Charan Sai Macha <165017283+charansai2255@users.noreply.github.com>
2026-06-09 11:48:01 +0530

    Update predictionApi.js

--- frontend/src/services/predictionApi.js
+++ frontend/src/services/predictionApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://car-price-api-tqnv.onrender.com/";
 
 export const predictPrice = async (data) => {
   const response = await axios.post(
